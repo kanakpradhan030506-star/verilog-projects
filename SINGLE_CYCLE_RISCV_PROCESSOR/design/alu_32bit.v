b2e533b0e203ce933cf3a7091fc733482d152c06
kanakpradhan030506-star <kanakpradhan030506@gmail.com>
2026-06-22 20:09:53 +0530

    Added complete single cycle RISC-V processor project

---
+++ SINGLE_CYCLE_RISCV_PROCESSOR/design/alu_32bit.v
@@ -0,0 +1,16 @@
+module alu_32bit(input [31:0] A , input [31:0] B , input [2:0] alu_sel , output reg [:0] result);
+  always @(*)
+    begin 
+    case(alu_sel) 
+      3'b000 : result = A + B; //ADDITION
+      3'b001 : result = A - B; //SUBTRACTION
+      3'b010 : result = A & B; // AND
+      3'b011 : result = A | B; //OR
+      3'b100 : result = A ^ B; //XOR
+      3'b101 : result = ~A ; // NOT A
+      3'b110 : result = A << 1; //LEFT SHIFT
+      3'b111 : result = A >> 1; //RIGHT SHIFT
+      default: result = 8'b00000000;
+    endcase
+    end
+endmodule
